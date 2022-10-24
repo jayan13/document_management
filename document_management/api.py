a53fd071a67b2965996d8553785b8a275fc4b8fd
jayan13 <jayakumar13s@gmail.com>
2022-10-24 12:21:20 +0530

    creadit limit

--- document_management/api.py
+++ document_management/api.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from distutils.log import debug
 import frappe, erpnext
 from frappe.utils import flt, cstr, nowdate, comma_and, cint, getdate
 from frappe import throw, msgprint, _
@@ -12,6 +13,6 @@ from frappe import throw, msgprint, _
 def get_limit(company,customer):
 
 	#emprv=frappe.db.sql(""" select * from `tabExpense Request Item` where docstatus=1 and employee='{0}' and date between DATE_SUB('{2}', INTERVAL {1} YEAR) and '{2}' """.format(employee,exp_year,exp_date),as_dict=1,debug=0)
-	return frappe.db.get_value('Customer Credit Forms', {'company': company,'parent':customer}, ['credit_limit']) or 0
+	return frappe.db.get_value('Customer Credit Forms', {'company': company,'parent':customer}, ['credit_limit'],debug=1) or 0
 	
 	
